deded2af02670c487e3af2faaddced6edaca7c59
pedropark99 <pedropark99@gmail.com>
2021-07-20 21:32:21 -0300

    create docs path

--- knit_book.R
+++ knit_book.R
@@ -1,4 +1,20 @@
 library(bookdown)
 library(bslib)
+library(fs)
 
 render_book(output_format = "bookdown::bs4_book")
+
+
+arquivos <- dir_ls("_book/")
+nomes <- path_file(arquivos)
+
+dir_create("docs")
+
+
+fs::file_move(
+  path = arquivos,
+  new_path = paste0("docs/", nomes)
+)
+
+
+dir_delete("_book/")
